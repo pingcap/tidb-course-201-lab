159ac52e0b2c5005e0eccd20d1140fdfab04085c
Guanglei BAO <toddbgl@hotmail.com>
2022-04-17 22:23:18 +0800

    ch07 recorded

--- scripts/07-demo-tiflash-olap-01-show.sql
+++ scripts/07-demo-tiflash-olap-01-show.sql
@@ -1,10 +1,20 @@
-/* source 07-demo-tiflash-olap-01-setup.sql */
+/* source 07-demo-tiflash-olap-01-show.sql */
+
+/* Reset */
+set @@tidb_isolation_read_engines='tikv,tiflash,tidb';
+ALTER TABLE test.t2_nonclustered SET TIFLASH REPLICA 0;
+ALTER TABLE test.t5_nonclustered SET TIFLASH REPLICA 0;
+
+/* WHERE for analysis */
+
+SELECT COUNT(*) FROM test.t5_nonclustered;
+SELECT COUNT(*) FROM test.t2_nonclustered;
 
 /* OLAP Style Query on t2 and t5 */
 SELECT avg(t5.id), t5.id2, count(*) 
 FROM test.t5_nonclustered t5 JOIN test.t2_nonclustered t2
 ON t5.id = t2.id
-WHERE t5.id > t5.id2
+WHERE t5.id < t2.id2
 GROUP BY id2
 ORDER BY id2;
 
@@ -16,18 +26,27 @@ ALTER TABLE test.t5_nonclustered SET TIFLASH REPLICA 1;
 SELECT avg(t5.id), t5.id2, count(*) 
 FROM test.t5_nonclustered t5 JOIN test.t2_nonclustered t2
 ON t5.id = t2.id
-WHERE t5.id > t5.id2
+WHERE t5.id < t2.id2
+GROUP BY id2
+ORDER BY id2;
+
+/* Explain */
+EXPLAIN SELECT avg(t5.id), t5.id2, count(*) 
+FROM test.t5_nonclustered t5 JOIN test.t2_nonclustered t2
+ON t5.id = t2.id
+WHERE t5.id < t2.id2
 GROUP BY id2
 ORDER BY id2;
 
 /* Turn off TiFlash for sesion */
+select @@tidb_isolation_read_engines;
 set @@tidb_isolation_read_engines='tikv,tidb';
 
 /* OLAP Style Query on t2 and t5 */
 SELECT avg(t5.id), t5.id2, count(*) 
 FROM test.t5_nonclustered t5 JOIN test.t2_nonclustered t2
 ON t5.id = t2.id
-WHERE t5.id > t5.id2
+WHERE t5.id < t2.id2
 GROUP BY id2
 ORDER BY id2;
 
