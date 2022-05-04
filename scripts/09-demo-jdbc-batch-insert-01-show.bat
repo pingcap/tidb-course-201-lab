f27e2abfea39c5ceed740fed5cc2fe072cfc60bd
Guanglei BAO <toddbgl@hotmail.com>
2022-05-04 22:49:41 +0800

    adding Windows bat to ch09

---
+++ scripts/09-demo-jdbc-batch-insert-01-show.bat
@@ -0,0 +1,5 @@
+del DemoJdbcBatchInsert.class
+
+javac -cp .;misc/mysql-connector-java-5.1.36-bin.jar DemoJdbcBatchInsert.java
+sleep 1
+java -cp .;misc/mysql-connector-java-5.1.36-bin.jar DemoJdbcBatchInsert
---
+++ scripts/09-demo-jdbc-prepared-statement-01-show.bat
@@ -0,0 +1,5 @@
+del DemoJdbcPreparedStatement.class
+
+javac -cp .;misc/mysql-connector-java-5.1.36-bin.jar DemoJdbcPreparedStatement.java
+sleep 1
+java -cp .;misc/mysql-connector-java-5.1.36-bin.jar DemoJdbcPreparedStatement
