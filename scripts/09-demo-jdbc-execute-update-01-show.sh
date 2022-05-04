ebab668f36e522b7a4a0b9716bb540ee972eab93
Guanglei BAO <toddbgl@hotmail.com>
2022-05-04 17:14:03 +0800

    ./09-demo-jdbc-execute-update-01-show.bat  added

--- scripts/09-demo-jdbc-execute-update-01-show.sh
+++ scripts/09-demo-jdbc-execute-update-01-show.sh
@@ -5,4 +5,4 @@
 rm -f DemoJdbcExecuteUpdate.class
 javac -cp .:misc/mysql-connector-java-5.1.36-bin.jar DemoJdbcExecuteUpdate.java
 sleep 1
-java -cp .:misc/mysql-connector-java-5.1.36-bin.jar  DemoJdbcExecuteUpdate
+java -cp .:misc/mysql-connector-java-5.1.36-bin.jar DemoJdbcExecuteUpdate
