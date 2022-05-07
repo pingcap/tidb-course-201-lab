a91b5c62e00577b11a5aaaf4d866fd8db5c83382
Guanglei BAO <toddbgl@hotmail.com>
2022-05-07 18:29:47 +0800

    adding char_length

--- scripts/03-demo-charset-01-show.sql
+++ scripts/03-demo-charset-01-show.sql
@@ -3,6 +3,7 @@
 /* Chinese Characters */
 SELECT
     length('曹操') as Byte_Length,
+    char_length('曹操') as Char_Length,
     '曹操' as Chinese,
     cast('曹操' as CHAR character set gbk) as GBK_ENCODED,
     cast('曹操' as CHAR character set utf8mb4) as UTF8MB4_ENCODED,
@@ -16,6 +17,7 @@ SELECT
 /* Japanese Characters */
 SELECT
     length('うぬぼれ刑事') as Byte_Length,
+    char_length('うぬぼれ刑事') as Char_Length,
     'うぬぼれ刑事' as Japanese,
     cast('うぬぼれ刑事' as CHAR character set gbk) as GBK_ENCODED,
     cast('うぬぼれ刑事' as CHAR character set utf8mb4) as UTF8MB4_ENCODED,
@@ -29,6 +31,7 @@ SELECT
 /* English Characters */
 SELECT
     length('Supreme Commander') as Byte_Length,
+    char_length('Supreme Commander') as Char_Length,
     'Supreme Commander' as English,
     cast('Supreme Commander' as CHAR character set gbk) as GBK_ENCODED,
     cast('Supreme Commander' as CHAR character set utf8mb4) as UTF8MB4_ENCODED,
