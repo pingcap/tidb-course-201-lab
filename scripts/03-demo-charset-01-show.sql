4ac786fb6c5f053bb7cf0dec2814c0978b8e9777
Guanglei BAO <toddbgl@hotmail.com>
2022-07-01 09:21:57 +0800

    minor update

--- scripts/03-demo-charset-01-show.sql
+++ scripts/03-demo-charset-01-show.sql
@@ -1,17 +1,17 @@
 /* source 03-demo-charset-01-show.sql */
 
-/* Chinese Characters */
+/* English Characters */
 SELECT
-    length('为磁盘溢出启用加密') as Byte_Length,
-    char_length('为磁盘溢出启用加密') as Char_Length,
-    '为磁盘溢出启用加密' as Chinese,
-    cast('为磁盘溢出启用加密' as CHAR character set gbk) as GBK_ENCODED,
-    cast('为磁盘溢出启用加密' as CHAR character set utf8mb4) as UTF8MB4_ENCODED,
+    length('Enable Encryption for Disk Spill') as Byte_Length,
+    char_length('Enable Encryption for Disk Spill') as Char_Length,
+    'Enable Encryption for Disk Spill' as English,
+    cast('Enable Encryption for Disk Spill' as CHAR character set gbk) as GBK_ENCODED,
+    cast('Enable Encryption for Disk Spill' as CHAR character set utf8mb4) as UTF8MB4_ENCODED,
     cast(
-        cast('为磁盘溢出启用加密' as CHAR character set gbk) 
+        cast('Enable Encryption for Disk Spill' as CHAR character set gbk) 
     as binary) as GBK_BINARY,
     cast(
-        cast('为磁盘溢出启用加密' as CHAR character set utf8mb4)
+        cast('Enable Encryption for Disk Spill' as CHAR character set utf8mb4)
     as binary) as UTF8MB4_BINARY\G
 
 /* Japanese Characters */
@@ -28,16 +28,16 @@ SELECT
         cast('ディスク流出時の暗号化機能を有効にする' as CHAR character set utf8mb4)
     as binary) as UTF8MB4_BINARY\G
 
-/* English Characters */
+/* Chinese Characters */
 SELECT
-    length('Enable Encryption for Disk Spill') as Byte_Length,
-    char_length('Enable Encryption for Disk Spill') as Char_Length,
-    'Enable Encryption for Disk Spill' as English,
-    cast('Enable Encryption for Disk Spill' as CHAR character set gbk) as GBK_ENCODED,
-    cast('Enable Encryption for Disk Spill' as CHAR character set utf8mb4) as UTF8MB4_ENCODED,
+    length('为磁盘溢出启用加密') as Byte_Length,
+    char_length('为磁盘溢出启用加密') as Char_Length,
+    '为磁盘溢出启用加密' as Chinese,
+    cast('为磁盘溢出启用加密' as CHAR character set gbk) as GBK_ENCODED,
+    cast('为磁盘溢出启用加密' as CHAR character set utf8mb4) as UTF8MB4_ENCODED,
     cast(
-        cast('Enable Encryption for Disk Spill' as CHAR character set gbk) 
+        cast('为磁盘溢出启用加密' as CHAR character set gbk) 
     as binary) as GBK_BINARY,
     cast(
-        cast('Enable Encryption for Disk Spill' as CHAR character set utf8mb4)
+        cast('为磁盘溢出启用加密' as CHAR character set utf8mb4)
     as binary) as UTF8MB4_BINARY\G
