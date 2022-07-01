f6363e0a89cb8d1a554f6b71acc237a7406df95a
Guanglei BAO <toddbgl@hotmail.com>
2022-07-01 11:30:02 +0800

    minor update

--- scripts/demo-data-type-maxlength.py
+++ scripts/demo-data-type-maxlength.py
@@ -1,5 +1,6 @@
 from mysql.connector import connect
 from mysql.connector.errors import DataError as ex
+from mysql.connector.errors import ProgrammingError as perr
 
 port = 4000
 conn = connect(
@@ -12,29 +13,52 @@ conn = connect(
 print('Connected to TiDB port:',port)
 cursor = conn.cursor(prepared=True)
 
-def _setup():
+def _setup(try_char_def, try_varchar_def):
   ps_drop_table = \
     '''
     DROP TABLE IF EXISTS dyc
     '''
-  ps_create_table = \
+  max_char_def = try_char_def
+  max_varchar_def = try_varchar_def
+  dyc_creation_ddl_stmt = \
     '''
-    CREATE TABLE IF NOT EXISTS dyc (
-      name varchar(100),
-      max_tinytext TINYTEXT,
-      max_text TEXT,
-      max_mediumtext MEDIUMTEXT,
-      max_longtext LONGTEXT,
-      max_tinyblob TINYBLOB,
-      max_blob BLOB,
-      max_char CHAR(255),
-      max_varchar VARCHAR(16383)
-    )
-    '''
-  
-  cursor.execute(ps_drop_table)
+        CREATE TABLE IF NOT EXISTS dyc (
+          name varchar(100),
+          max_tinytext TINYTEXT,
+          max_text TEXT,
+          max_mediumtext MEDIUMTEXT,
+          max_longtext LONGTEXT,
+          max_tinyblob TINYBLOB,
+          max_blob BLOB,
+          max_char CHAR({}),
+          max_varchar VARCHAR({})
+        )
+        '''
+  # Test CHAR
+  while True:
+    try:
+      cursor.execute(ps_drop_table)
+      ps_create_table = dyc_creation_ddl_stmt.format(try_char_def, try_varchar_def)
+      cursor.execute(ps_create_table)
+      max_char_def = try_char_def
+      try_char_def += 1
+    except perr:
+      break
+  # Test VARCHAR
+  while True:
+    try:
+      cursor.execute(ps_drop_table)
+      ps_create_table = dyc_creation_ddl_stmt.format(max_char_def, try_varchar_def)
+      cursor.execute(ps_create_table)
+      max_varchar_def = try_varchar_def
+      try_varchar_def += 1
+    except perr:
+      break
+  # Final
+  ps_create_table = dyc_creation_ddl_stmt.format(max_char_def, max_varchar_def)
   cursor.execute(ps_create_table)
   cursor.execute('SET @@tidb_mem_quota_query = 4 << 30') # 4GB of TiDB query memory
+  return (max_char_def,max_varchar_def)
 
 def _check():
   cursor.execute(
@@ -54,83 +78,81 @@ def _check():
 
 def _execute_char(insert_statement: str, update_statement: str, meta_char: str, start_len):
   cursor.execute(
-            "INSERT INTO dyc (name, max_tinytext) VALUES ('TINYTEXT', %s)",
-            ("A"*255)
+            insert_statement,
+            (meta_char*start_len,)
           )
   conn.commit()
   max_length = start_len
   while True:
-    max_length += 1
-    cursor.execute(
-      update_statement,
-      (meta_char*max_length)
-    )
-    conn.commit()
+    try:
+      try_length = max_length + 1
+      cursor.execute(
+        update_statement,
+        (meta_char*try_length,)
+      )#
+      conn.commit()
+      max_length += 1
+    except ex:
+      break
 
-def _tinytextv2():
+def _tinytext():
   _execute_char(
             insert_statement="INSERT INTO dyc (name, max_tinytext) VALUES ('TINYTEXT', %s)",
             update_statement="UPDATE dyc SET max_tinytext = %s WHERE name = 'TINYTEXT'",
             meta_char="A",
-            start_len=254
+            start_len=250
           )
 
-def _tinytext(name: str):
-  max_length = 255
-  cursor.execute( 
-            "INSERT INTO dyc (name, max_tinytext) VALUES (%s, %s)",
-            (name, "A"*max_length)
+def _text():
+  _execute_char( 
+            "INSERT INTO dyc (name, max_text) VALUES ('TEXT', %s)",
+            "UPDATE dyc SET max_text = %s WHERE name = 'TEXT'",
+            meta_char="A",
+            start_len=65530
           )
-  conn.commit()
 
-def _text(name: str):
-  max_length = 65535
-  cursor.execute( 
-            "INSERT INTO dyc (name, max_text) VALUES (%s, %s)",
-            (name, "A"*max_length)
+def _tinyblob():
+  _execute_char(
+            "INSERT INTO dyc (name, max_tinyblob) VALUES ('TINYBLOB', binary(%s))",
+            "UPDATE dyc SET max_tinyblob = binary(%s) WHERE name = 'TINYBLOB'",
+            meta_char="A",
+            start_len=250
           )
-  conn.commit()
 
-def _tinyblob(name: str):
-  max_length = 255
-  cursor.execute( 
-            "INSERT INTO dyc (name, max_tinyblob) VALUES (%s, binary(%s))",
-            (name, "A"*max_length)
+def _blob():
+  _execute_char(
+            "INSERT INTO dyc (name, max_blob) VALUES ('BLOB', binary(%s))",
+            "UPDATE dyc SET max_blob = binary(%s) WHERE name = 'BLOB'",
+            meta_char="A",
+            start_len=65530
           )
-  conn.commit()
 
-def _blob(name: str):
-  max_length = 65535
-  cursor.execute( 
-            "INSERT INTO dyc (name, max_blob) VALUES (%s, binary(%s))",
-            (name, "A"*max_length)
+def _char(max_char_def):
+  _execute_char(
+            "INSERT INTO dyc (name, max_char) VALUES ('CHAR("+str(max_char_def)+")', %s)",
+            "UPDATE dyc SET max_char = %s WHERE name = 'CHAR("+str(max_char_def)+")'",
+            meta_char="我",
+            start_len=250
           )
-  conn.commit()
 
-def _char(name: str):
-  max_length = 255
-  cursor.execute( 
-            "INSERT INTO dyc (name, max_char) VALUES (%s, %s)",
-            (name, "我"*max_length)
+def _varchar(max_varchar_def):
+  _execute_char(
+            "INSERT INTO dyc (name, max_varchar) VALUES ('VARCHAR("+str(max_varchar_def)+")', %s)",
+            "UPDATE dyc SET max_varchar = %s WHERE name = 'VARCHAR("+str(max_varchar_def)+")'",
+            meta_char="我",
+            start_len=16380
           )
-  conn.commit()
 
-def _varchar(name: str):
-  max_length = 16383
-  cursor.execute( 
-            "INSERT INTO dyc (name, max_varchar) VALUES (%s, %s)",
-            (name, "我"*max_length)
-          )
-  conn.commit()
-
-_setup()
-_tinytext("TINYTEXT")
-_text("TEXT")
-_tinyblob("TINYBLOB")
-_blob("BLOB")
-_char("CHAR(255)")
-_varchar("VARCHAR(16383)")
+# Main
+max_def = _setup(try_char_def = 253, try_varchar_def = 16382)
+_char(max_def[0])
+_varchar(max_def[1])
+_tinytext()
+_text()
+_tinyblob()
+_blob()
 _check()
 
+# Mr. Proper
 cursor.close()
 conn.close()
