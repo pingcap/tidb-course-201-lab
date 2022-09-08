161434b315d6c5cc29a19a7535507a857755da52
Guanglei BAO <toddbgl@hotmail.com>
2022-09-08 17:01:30 +0800

    high-cap updated

--- scripts/playground-start-high-cap.sh
+++ scripts/playground-start-high-cap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tiup playground v5.4.0 --tag classroom-high-cap --db 3 --pd 3 --kv 3 --tiflash 1 \
+tiup playground v5.4.0 --tag classroom-high-cap --db 1 --pd 1 --kv 1 --tiflash 1 \
 --kv.config ./misc/tikv-txn.toml \
 --db.config ./misc/tidb-txn.toml
