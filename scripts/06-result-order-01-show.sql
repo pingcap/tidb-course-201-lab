784d28371934d62f6ebdded94cc53316603868e2
Guanglei BAO <toddbgl@hotmail.com>
2022-09-18 17:28:28 +0800

    minor update

--- scripts/06-result-order-01-show.sql
+++ scripts/06-result-order-01-show.sql
@@ -34,7 +34,11 @@ INSERT INTO test.ro_example SELECT null, content FROM test.ro_example;
 INSERT INTO test.ro_example SELECT null, content FROM test.ro_example;
 
 /* Multiple TiKV regions exist, your regions count might be 4. (We need multiple regions to show the expected behavior.) */
-SHOW TABLE test.ro_example REGIONS\G
+/* SHOW TABLE test.ro_example REGIONS\G */
+
+/* Are following results the same? Why? Observe the execution plan. */
+EXPLAIN
+SELECT pk, row_number() over() FROM test.ro_example ORDER BY pk LIMIT 2;
 
 /* Observe the result 1 */
 SELECT pk, row_number() over() FROM test.ro_example ORDER BY pk LIMIT 2;
@@ -42,25 +46,28 @@ SELECT pk, row_number() over() FROM test.ro_example ORDER BY pk LIMIT 2;
 SELECT pk, row_number() over() FROM test.ro_example ORDER BY pk LIMIT 2;
 /* Observe the result 3 */
 SELECT pk, row_number() over() FROM test.ro_example ORDER BY pk LIMIT 2;
-
-/* Are above result the same? Why? Observe the execution plan. */
-EXPLAIN
+/* Observe the result 4 */
 SELECT pk, row_number() over() FROM test.ro_example ORDER BY pk LIMIT 2;
 
+
+/* Are following results the same? Why? Observe the execution plan. */
+EXPLAIN
+SELECT pk, row_number() over() FROM
+(SELECT pk FROM test.ro_example ORDER BY pk LIMIT 2) v;
 
 /* Try again. */
 /* Observe the result 1 */
 SELECT pk, row_number() over() FROM
 (SELECT pk FROM test.ro_example ORDER BY pk LIMIT 2) v;
-/* Observe the result 1 */
+/* Observe the result 2 */
 SELECT pk, row_number() over() FROM
 (SELECT pk FROM test.ro_example ORDER BY pk LIMIT 2) v;
-/* Observe the result 1 */
+/* Observe the result 3 */
+SELECT pk, row_number() over() FROM
+(SELECT pk FROM test.ro_example ORDER BY pk LIMIT 2) v;
+/* Observe the result 4 */
 SELECT pk, row_number() over() FROM
 (SELECT pk FROM test.ro_example ORDER BY pk LIMIT 2) v;
 
-/* Are above result the same? Why? Observe the execution plan. */
-EXPLAIN
-SELECT pk, row_number() over() FROM
-(SELECT pk FROM test.ro_example ORDER BY pk LIMIT 2) v;
+
 
