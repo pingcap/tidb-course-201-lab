55ff78d2a1a8c2f8f4e15446b7e9d1a00f50814b
Guanglei BAO <toddbgl@hotmail.com>
2022-11-24 08:44:57 +0800

    adding TiFlash back to 1-node ENV

--- setup/tidb-lab-aws-1-node-amz2/02-precheck-and-fix-node.sh
+++ setup/tidb-lab-aws-1-node-amz2/02-precheck-and-fix-node.sh
@@ -2,6 +2,6 @@
 ~/.tiup/bin/tiup cluster check single-node-hybrid.yaml --apply && \
 ~/.tiup/bin/tiup cluster check single-node-hybrid.yaml
 
-echo
-echo "## It's safe to ignore disk and cpu-governor check failures in this classroom environment."
-echo
+echo "############################################################################################"
+echo "## It's safe to ignore disk and cpu-governor check failures in this classroom environment. #"
+echo "############################################################################################"
--- setup/tidb-lab-aws-4-node-amz2/02-precheck-and-fix-nodes.sh
+++ setup/tidb-lab-aws-4-node-amz2/02-precheck-and-fix-nodes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-~/.tiup/bin/tiup cluster check four-nodes-hybrid.yaml --apply && \
-~/.tiup/bin/tiup cluster check four-nodes-hybrid.yaml
+~/.tiup/bin/tiup cluster check single-node-hybrid.yaml --apply && \
+~/.tiup/bin/tiup cluster check single-node-hybrid.yaml
 
-echo
-echo "## It's safe to ignore disk and cpu-governor check failures in this classroom environment."
-echo
+echo "############################################################################################"
+echo "## It's safe to ignore disk and cpu-governor check failures in this classroom environment. #"
+echo "############################################################################################"
