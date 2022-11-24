d25def2e9d1fde77c823a69f8e307f2a17dcbc8e
Guanglei BAO <toddbgl@hotmail.com>
2022-11-24 19:48:28 +0800

    fix

--- setup/tidb-lab-aws-4-node-amz2/02-precheck-and-fix-nodes.sh
+++ setup/tidb-lab-aws-4-node-amz2/02-precheck-and-fix-nodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-~/.tiup/bin/tiup cluster check single-node-hybrid.yaml --apply && \
-~/.tiup/bin/tiup cluster check single-node-hybrid.yaml
+~/.tiup/bin/tiup cluster check four-nodes-hybrid.yaml --apply && \
+~/.tiup/bin/tiup cluster check four-nodes-hybrid.yaml
 
 echo "############################################################################################"
 echo "## It's safe to ignore disk and cpu-governor check failures in this classroom environment. #"
