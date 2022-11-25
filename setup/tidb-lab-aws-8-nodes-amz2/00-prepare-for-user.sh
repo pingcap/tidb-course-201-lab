4b8823c2c1176843d3f06e0b96d432352c997041
Guanglei BAO <toddbgl@hotmail.com>
2022-11-25 09:33:47 +0800

    8 nodes lab ENV setup

--- setup/tidb-lab-aws-8-nodes-amz2/00-prepare-for-user.sh
+++ setup/tidb-lab-aws-8-nodes-amz2/00-prepare-for-user.sh
@@ -5,108 +5,110 @@ source ~/cloud-env.sh
 
 # Enter the classroom username such as "user01" assigned by the instructor. 
 export USER=${1}
+# The instructor's name tag shared by your instructor.
+export TRAINER=${2}
 
 # Setup PD1,2,3 as all node roles (4 ASGs: 1 ASG for PD, 1 ASG for TiFlash, 1 ASG for KV, 1 ASG for DB)
 # Node 1
 HOST_PD1_PRIVATE_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd1" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PrivateIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 HOST_PD1_PUBLIC_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd1" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PublicIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 # Node 2
 HOST_PD2_PRIVATE_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd2" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd2" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PrivateIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 HOST_PD2_PUBLIC_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd2" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd2" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PublicIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 # Node 3
 HOST_PD3_PRIVATE_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd3" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd3" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PrivateIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 HOST_PD3_PUBLIC_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd3" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd3" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PublicIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 # Node 4
 HOST_TIFLASH1_PRIVATE_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=tiflash1" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=tiflash1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PrivateIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 HOST_TIFLASH1_PUBLIC_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=tiflash1" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=tiflash1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PublicIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 # Node 5
 HOST_KV1_PRIVATE_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv1" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PrivateIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 HOST_KV1_PUBLIC_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv1" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PublicIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 # Node 6
 HOST_KV2_PRIVATE_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv2" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv2" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PrivateIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 HOST_KV2_PUBLIC_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv2" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv2" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PublicIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 # Node 7
 HOST_KV3_PRIVATE_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv3" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv3" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PrivateIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 HOST_KV3_PUBLIC_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv3" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=kv3" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PublicIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 # Node 8
 HOST_DB1_PRIVATE_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=db1" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=db1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PrivateIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 HOST_DB1_PUBLIC_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=db1" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=db1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PublicIpAddress" \
 --output text \
 --region ${REGION_CODE}`
@@ -157,7 +159,7 @@ sed -i '' \
   -e "s/<HOST_KV2_PRIVATE_IP>/${HOST_KV2_PRIVATE_IP}/g" \
   -e "s/<HOST_KV3_PRIVATE_IP>/${HOST_KV3_PRIVATE_IP}/g" \
   -e "s/<HOST_DB1_PRIVATE_IP>/${HOST_DB1_PRIVATE_IP}/g" \
-  -e "s/<HOST_DB2_PRIVATE_IP>/${HOST_DB1_PRIVATE_IP}/g" \
+  -e "s/<HOST_DB2_PRIVATE_IP>/${HOST_DB2_PRIVATE_IP}/g" \
   ./eight-nodes-hybrid.yaml 2>/dev/null
 
 echo Topology config file for four nodes cluster prepared.
