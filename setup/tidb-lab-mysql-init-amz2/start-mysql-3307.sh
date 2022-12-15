2ece8d6a072747c40708f310bf9f9dbabd6b9cd4
1793033023 <1793033023@qq.com>
2022-12-15 19:30:02 +0800

    modify

--- setup/tidb-lab-mysql-init-amz2/start-mysql-3307.sh
+++ setup/tidb-lab-mysql-init-amz2/start-mysql-3307.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo /usr/local/mysql/bin/mysqld_safe --defaults-file=~/stage/tidb-course-201-lab/setup/tidb-lab-mysql-init-amz2/my3307.cnf &
+sudo /usr/local/mysql/bin/mysqld_safe --defaults-file=/home/ec2-user/stage/tidb-course-201-lab/setup/tidb-lab-mysql-init-amz2/my3307.cnf &
