58bb496cc673047f9907bb41f2860d90f0500ba3
Guanglei BAO <toddbgl@hotmail.com>
2023-01-12 15:29:12 +0800

    minor update

--- scripts/07-demo-auto-increment-01-setup.sql
+++ scripts/07-demo-auto-increment-01-setup.sql
@@ -3,15 +3,15 @@
 /* show variables like 'auto_increment_%'; */
 /* show variables like 'tidb_allow_remove_auto_inc'; */
 
-/* Setup the schema for attribute AUTO_INCREMENT */
+/* Setup: the schema for attribute AUTO_INCREMENT */
 DROP TABLE IF EXISTS test.t1;
 CREATE TABLE test.t1 (
-    id int PRIMARY KEY AUTO_INCREMENT, 
-    from_port char(4));
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    from_port CHAR(4))
+    AUTO_ID_CACHE 300;
 
 DROP TABLE IF EXISTS test.t2;
 CREATE TABLE test.t2 (
-    id int PRIMARY KEY AUTO_INCREMENT, 
-    from_port char(4))
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    from_port CHAR(4))
     AUTO_ID_CACHE 300;
-
--- scripts/07-demo-auto-increment-02-show.sql
+++ scripts/07-demo-auto-increment-02-show.sql
@@ -1,18 +1,10 @@
 /* source 07-demo-auto-increment-02-show.sql */
 
-/* Populate */
-INSERT INTO test.t1 (from_port) VALUES ('4000'), ('4000'), ('4000');
+/* Populate test.t1 */
+INSERT INTO test.t1 (from_port) VALUES ('4000');
+INSERT INTO test.t1 (from_port) VALUES ('4000');
+INSERT INTO test.t1 (from_port) VALUES ('4000');
+INSERT INTO test.t1 (from_port) VALUES ('4000');
 
-/* Check value */
-select id, from_port from test.t1;
-
-/* Explictly assign value "7" to auto_increment column */
-insert into test.t1 values (7, '4000');
-/* Check value */
-select id, from_port from test.t1;
-
-/* Relying on auto_increment values to assign values to new rows */
-insert into test.t1 (from_port) values ('4000');
-insert into test.t1 (from_port) values ('4000');
-/* Check value */
-select id, from_port from test.t1;
+/* Query 1: Check */
+SELECT id, from_port FROM test.t1;
