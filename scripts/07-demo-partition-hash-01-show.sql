6adc6558af1d96cb43db8f7fd8c5a452f0be45fc
Guanglei BAO <toddbgl@hotmail.com>
2023-01-12 20:14:31 +0800

    minor update

--- scripts/07-demo-partition-hash-01-show.sql
+++ scripts/07-demo-partition-hash-01-show.sql
@@ -1,46 +1,35 @@
 /* source 07-demo-partition-hash-01-show.sql */
 
-/* Hash Partition t1 */
-drop table if exists test.t1;
+/* Setup: Create a hash partitioned table t1 */
+DROP TABLE IF EXISTS test.t1;
 CREATE TABLE test.t1 (x INT)
     PARTITION BY HASH(x)
     PARTITIONS 4;
 
-insert into test.t1 values (0),(1),(2),(3),(4),(5),(6),(7),(8),(9);
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
+/* Populate dummy data */
+INSERT INTO test.t1 VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9);
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+ANALYZE TABLE test.t1;
 
+/* Query 1: Check partition point queries with equi predicate */
+EXPLAIN SELECT * FROM test.t1 where x=0;
+EXPLAIN SELECT * FROM test.t1 where x=2;
+EXPLAIN SELECT * FROM test.t1 where x=9;
 
-/* Check Partition Distribution */
-explain select * from test.t1 where x=0;
-explain select * from test.t1 where x=1;
-explain select * from test.t1 where x=2;
-explain select * from test.t1 where x=3;
-explain select * from test.t1 where x=4;
-explain select * from test.t1 where x=5;
-explain select * from test.t1 where x=6;
-explain select * from test.t1 where x=7;
-explain select * from test.t1 where x=8;
-explain select * from test.t1 where x=9;
-
-/* Negative */
-explain select * from test.t1 where x between 7 and 9;
-
-/* Check regions */
-show table test.t1 regions;
+/* Query 2: Check multiple partitions query with range preficate */
+EXPLAIN SELECT * FROM test.t1 where x between 7 and 9;
--- scripts/07-demo-partition-range-01-show.sql
+++ scripts/07-demo-partition-range-01-show.sql
@@ -1,34 +1,39 @@
 /* source 07-demo-partition-range-01-show.sql */
 
-/* Range Partition t1 */
-drop table if exists test.t1;
-create table test.t1 (x int) partition by range (x) (
-    partition p0 values less than (5),
-    partition p1 values less than (10));
+/* Setup: Create a range partitioned table t1 */
+DROP TABLE IF EXISTS test.t1;
+CREATE TABLE test.t1 (x INT) PARTITION BY RANGE (x) (
+    PARTITION p0 VALUES LESS THAN (5),
+    PARTITION p1 VALUES LESS THAN (10),
+    PARTITION p2 VALUES LESS THAN (MAXVALUE));
 
-insert into test.t1 values (0),(1),(2),(3),(4),(5),(6),(7),(8),(9);
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
-insert into test.t1 select * from test.t1;
+/* Populate dummy data */
+INSERT INTO test.t1 VALUES (0),(1),(2),(3),(4),(5),(6),(7),(8),(9);
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+INSERT INTO test.t1 SELECT * FROM test.t1;
+ANALYZE TABLE test.t1;
 
+/* Query 1: Check partition pruning with equi predicate */
+EXPLAIN SELECT * FROM test.t1 where x=0;
+EXPLAIN SELECT * FROM test.t1 where x=2;
+EXPLAIN SELECT * FROM test.t1 where x=9;
 
-/* Check Partition Pruning */
-explain select * from test.t1 where x between 1 and 4;
+/* Query 2: Check partition pruning with range predicate */
+EXPLAIN SELECT * FROM test.t1 where x between 7 and 9;
 
-/* Check regions */
-show table test.t1 regions;
+/* Query 3: Check TiKV regions */
+SHOW TABLE test.t1 REGIONS\G
