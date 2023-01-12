4f6699e7b03a49a44d16205b2354511cf64b3906
Guanglei BAO <toddbgl@hotmail.com>
2023-01-12 13:24:33 +0800

    minor update

--- scripts/07-demo-global-temp-table.sql
+++ scripts/07-demo-global-temp-table.sql
@@ -1,23 +1,32 @@
 /* source 07-demo-global-temp-table.sql */
 
-/* create a global temporary table */
-DROP TABLE IF EXISTS test.temp2;
+/* Setup: Create a global temporary table */
+DROP TABLE IF EXISTS test.temp2, test.perm3;
+
+/* Create global temporary table attemp 1, fail with error code 1064 */
 CREATE GLOBAL TEMPORARY TABLE test.temp2 (id BIGINT);
+
+/* Create global temporary table attemp 2 */
 CREATE GLOBAL TEMPORARY TABLE test.temp2 (id BIGINT) ON COMMIT DELETE ROWS;
 
-/* set autocommit to off */
+/* Turn off autocommit for current session */
 SET @@autocommit = 'OFF';
 
-/* insert data into the global temporary table */
+/* Insert data into the global temporary table */
 INSERT INTO test.temp2 VALUES (10);
 
-/* check the data in the global temporary table */
+/* Query 1: Check the data in the global temporary table, expecting 10 */
 SELECT * FROM test.temp2;
 
+/* Manual commit, the data is cleared */
 COMMIT;
 
-/* check the data in the global temporary table again */
+/* Query 2: Check the data in the global temporary table again */
 SELECT * FROM test.temp2;
 
-/* check the global temporary table */
-SHOW CREATE TABLE test.temp2;
+/* Create a permanent table */
+CREATE TABLE test.perm3 (id BIGINT);
+
+/* Create global temporary table attemp 3, error code 1050 is expected */
+CREATE GLOBAL TEMPORARY TABLE test.perm3 (id BIGINT) ON COMMIT DELETE ROWS;
+
