2fa596a1f5736398ba678c40487f2faa502ab838
Chenhao Huang <chenhao.tony.huang@gmail.com>
2023-01-17 16:50:41 +1100

    demo timezone

---
+++ scripts/demo-timezone.sql
@@ -0,0 +1,29 @@
+/* source demo-timezone.sql */
+
+DROP TABLE IF EXISTS T;
+
+-- Query the global and session timezone 
+SELECT @@GLOBAL.TIME_ZONE, @@SESSION.TIME_ZONE;
+
+/* Query two timezone related functions */
+SELECT NOW();
+SELECT CURTIME();
+
+/* Create a table T with two columns, the type of the first column is one is DATETIME, and the other is TIMESTAMP */
+USE TEST;
+CREATE TABLE T(A DATETIME, B TIMESTAMP);
+INSERT INTO T VALUES(NOW(), NOW());
+SELECT * FROM T;
+
+/* Change the session timezone to UTC */
+SET SESSION TIME_ZONE='UTC';
+
+/* Query the global and session timezone */
+SELECT @@GLOBAL.TIME_ZONE, @@SESSION.TIME_ZONE;
+SELECT * FROM T;
+
+/* Query two timezone related functions */
+SELECT NOW();
+SELECT CURTIME();
+
+SELECT 'Query two timezone related functions ' as ' ';
