b7127e5ed08df158b923ddab87d8b93c22317b3e
Guanglei BAO <toddbgl@hotmail.com>
2023-01-28 19:36:51 +0800

    minor update

---
+++ setup/amz2-userdata/_00-userdata-for-dba-8-nodes-env-run-as-root.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script is for Amazon Linux 2.
+# Run this script as root user on an EC2 instance, or configure it as the USER_DATA to initialize an EC2 instance.  
+
+# Setup ENV
+echo LANG=en_US.utf-8 >> /etc/environment
+echo LC_ALL=en_US.utf-8 >> /etc/environment
+
+# Install
+mkdir ~ec2-user/stage/
+cd ~ec2-user/stage/
+rm -rf tidb-course-201-lab/
+yum -y update
+yum -y install git numactl awscli
+rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+wget https://dev.mysql.com/get/mysql57-community-release-el7-10.noarch.rpm
+yum -y install mysql57-community-release-el7-10.noarch.rpm
+yum -y install mysql-community-client
+wget https://pingcap-edu.s3.us-west-2.amazonaws.com/tidb-admin-dataset.zip
+git clone https://github.com/pingcap/tidb-course-201-lab.git
+
+# Service
+# systemctl restart ntpd.service
+
+# Setup Cloud ENV
+REGION_CODE=`curl http://169.254.169.254/latest/meta-data/placement/region`
+echo export REGION_CODE=${REGION_CODE} > ~ec2-user/cloud-env.sh
+
+# Copy required files
+alias cp='cp'
+cp -R tidb-course-201-lab/setup/tidb-lab-aws-8-nodes-amz2/* ~ec2-user/
+
+# Conslusion
+cd ~
+chown -R ec2-user:ec2-user ~ec2-user/*
+chmod +x ~ec2-user/*.sh
