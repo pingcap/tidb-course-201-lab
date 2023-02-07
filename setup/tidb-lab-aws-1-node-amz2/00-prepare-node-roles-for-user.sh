98d14c8be7feba8486b7fc14e8f236ccc722ffd6
Guanglei BAO <toddbgl@hotmail.com>
2023-02-07 16:10:49 +0800

    minor update

--- setup/tidb-lab-aws-1-node-amz2/00-prepare-node-roles-for-user.sh
+++ setup/tidb-lab-aws-1-node-amz2/00-prepare-node-roles-for-user.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+# Example: ./00-prepare-node-roles-for-user.sh <user_name_tag_assigned_by_instructor> <instructor_name_tag>
+
 # Setup ENV
 source ~/cloud-env.sh
 
-# Enter the classroom username such as "user01" assigned by the instructor. 
+# Enter the classroom username such as "user2" assigned by the instructor. 
 export USER=${1}
 # The instructor's name tag shared by your instructor.
 export TRAINER=${2}
 
-# Setup PD1 as all node roles (1 ASG for PD)
+# Setup PD1, KV1, DB1 and TiFlash1
 # Node 1
 HOST_PD1_PRIVATE_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd1" "Name=tag:trainer,Values=${TRAINER}" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:student,Values=${USER}" "Name=tag:role,Values=pd1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PrivateIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
 HOST_PD1_PUBLIC_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:class-${USER},Values=pd1" "Name=tag:trainer,Values=${TRAINER}" \
+--filter "Name=instance-state-name,Values=running" "Name=tag:student,Values=${USER}" "Name=tag:role,Values=pd1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PublicIpAddress" \
 --output text \
 --region ${REGION_CODE}`
@@ -36,8 +38,6 @@ echo export HOST_TIFLASH1_PRIVATE_IP=${HOST_PD1_PRIVATE_IP} >> ./hosts-env.sh
 echo export HOST_TIFLASH1_PUBLIC_IP=${HOST_PD1_PUBLIC_IP} >> ./hosts-env.sh
 echo export HOST_MONITOR_PRIVATE_IP=${HOST_PD1_PRIVATE_IP} >> ./hosts-env.sh
 echo export HOST_MONITOR_PUBLIC_IP=${HOST_PD1_PUBLIC_IP} >> ./hosts-env.sh
-echo export DASHBOARD_URL=http://${HOST_PD1_PUBLIC_IP}:2379/dashboard >> ./hosts-env.sh
-echo export GRAFANA_URL=http://${HOST_PD1_PUBLIC_IP}:3000/ >> ./hosts-env.sh
 
 source ./hosts-env.sh
 
@@ -45,13 +45,23 @@ echo ssh -A ${HOST_CM_PRIVATE_IP} > ./ssh-to-cm.sh
 echo ssh -A ${HOST_PD1_PRIVATE_IP} > ./ssh-to-pd1.sh
 echo ssh -A ${HOST_DB1_PRIVATE_IP} > ./ssh-to-db1.sh
 echo ssh -A ${HOST_DB2_PRIVATE_IP} > ./ssh-to-db2.sh
+echo ssh -A ${HOST_MONITOR_PRIVATE_IP} > ./ssh-to-monitor.sh
 echo ssh -A ${HOST_KV1_PRIVATE_IP} > ./ssh-to-kv1.sh
 echo ssh -A ${HOST_TIFLASH1_PRIVATE_IP} > ./ssh-to-tiflash1.sh
-echo ssh -A ${HOST_MONITOR_PRIVATE_IP} > ./ssh-to-monitor.sh
 chmod +x ./*.sh
 
-# Setup Single Node TiDB Topology
+# Setup Eight Nodes TiDB Cluster Topology
 cp ./template-single-node-hybrid.yaml ./single-node-hybrid.yaml
-sed -i '' -e "s/<HOST_PD1_PRIVATE_IP>/${HOST_PD1_PRIVATE_IP}/g" ./single-node-hybrid.yaml 2>/dev/null
+sed -i '' \
+  -e "s/<HOST_PD1_PRIVATE_IP>/${HOST_PD1_PRIVATE_IP}/g" \
+  -e "s/<HOST_TIFLASH1_PRIVATE_IP>/${HOST_TIFLASH1_PRIVATE_IP}/g" \
+  -e "s/<HOST_KV1_PRIVATE_IP>/${HOST_KV1_PRIVATE_IP}/g" \
+  -e "s/<HOST_DB1_PRIVATE_IP>/${HOST_DB1_PRIVATE_IP}/g" \
+  -e "s/<HOST_DB2_PRIVATE_IP>/${HOST_DB2_PRIVATE_IP}/g" \
+  ./single-node-hybrid.yaml 2>/dev/null
 
-echo Node is prepared.
+# Copy hosts-env.sh to user home. It's also a safe operation if the PWD is user home. 
+cp ./hosts-env.sh ~/hosts-env.sh 2>>/dev/null
+
+echo
+echo "8 nodes are prepared for user ${USER} and trainer ${TRAINER}."
