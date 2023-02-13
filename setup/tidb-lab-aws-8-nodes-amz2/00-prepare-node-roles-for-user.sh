e73033caf05c385a3bec904118a30b0e8785daf5
Guanglei BAO <toddbgl@hotmail.com>
2023-02-13 19:38:25 +0800

    minor update

--- setup/tidb-lab-aws-8-nodes-amz2/00-prepare-node-roles-for-user.sh
+++ setup/tidb-lab-aws-8-nodes-amz2/00-prepare-node-roles-for-user.sh
@@ -186,10 +186,12 @@ sed -i '' \
   ./sync-diff-config.toml 2>/dev/null
 
 # Setup template-single-node-scale-out-ticdc.yaml
-cp ./template-single-node-scale-out-ticdc.yaml ./single-node-scale-out-ticdc.yaml
+cp ./template-three-nodes-scale-out-ticdc.yaml ./three-nodes-scale-out-ticdc.yaml
 sed -i '' \
-  -e "s/<HOST_DB1_PRIVATE_IP>/${HOST_DB1_PRIVATE_IP}/g" \
-  ./single-node-scale-out-ticdc.yaml 2>/dev/null
+  -e "s/<HOST_PD1_PRIVATE_IP>/${HOST_PD1_PRIVATE_IP}/g" \
+  -e "s/<HOST_PD2_PRIVATE_IP>/${HOST_PD1_PRIVATE_IP}/g" \
+  -e "s/<HOST_PD3_PRIVATE_IP>/${HOST_PD1_PRIVATE_IP}/g" \
+  ./three-nodes-scale-out-ticdc.yaml 2>/dev/null
 
 # Copy hosts-env.sh to user home. It's also a safe operation if the PWD is user home. 
 cp ./hosts-env.sh ~/hosts-env.sh 2>>/dev/null
---
+++ setup/tidb-lab-aws-8-nodes-amz2/ff13-2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Fast forward E13-1
+./ff13-1.sh
+source .bash_profile
+source ./hosts-env.sh
+
+# Fast forward E13-2
+sudo service mysqld start
+X=`sudo grep 'temporary password' /var/log/mysqld.log | sed 's/.*: //'`
+mysql -h localhost -P 3306 -uroot -p$X --connect-expired-password << EOF
+ALTER USER root@'localhost' identified by 'q1w2e3R4_';
+CREATE USER cdc_user@'%' identified by 'q1w2e3R4_';
+GRANT ALL PRIVILEGES ON *.* TO cdc_user@'%';
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
+CREATE TABLE test.T1(id INT PRIMARY KEY, name VARCHAR(20));
+SELECT * FROM test.T1;
+EOF
+
+mysql -h${HOST_DB1_PRIVATE_IP} -P4000 -uroot << EOF
+CREATE TABLE test.T1(id INT PRIMARY KEY, name VARCHAR(20));
+SELECT * FROM test.T1;
+EOF
+
+tiup cdc cli changefeed create --pd=http://${HOST_PD1_PRIVATE_IP}:2379 \
+--sink-uri="mysql://cdc_user:q1w2e3R4_@${HOST_PD1_PRIVATE_IP}:3306/" \
+--changefeed-id="replication-task-1" --sort-engine="unified"
+
+tiup cdc cli changefeed list --pd=http://${HOST_PD1_PRIVATE_IP}:2379
+
+tiup cdc cli changefeed query --pd=http://${HOST_PD3_PRIVATE_IP}:2379 --changefeed-id=replication-task-1 
+
+mysql -h${HOST_DB1_PRIVATE_IP} -P4000 -uroot -p << EOF
+SELECT * FROM test.T1;
+INSERT INTO test.T1 VALUES(1, 'Tom');
+SELECT * FROM test.T1;
+EOF
+
+mysql -u cdc_user -h ${HOST_PD1_PRIVATE_IP} -P 3306 -pq1w2e3R4_ << EOF
+SELECT * FROM test.T1;
+EOF
+
