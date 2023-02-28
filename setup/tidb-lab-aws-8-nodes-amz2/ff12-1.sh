e917ec20545e90a01ee577fee7067d40fd6c3724
Guanglei BAO <toddbgl@hotmail.com>
2023-02-28 22:58:42 +0800

    minor update

--- setup/tidb-lab-aws-8-nodes-amz2/ff12-1.sh
+++ setup/tidb-lab-aws-8-nodes-amz2/ff12-1.sh
@@ -8,7 +8,7 @@ source ./hosts-env.sh
 # Fast forward E12-1
 tiup install dm dmctl
 tiup update --self && tiup update dm && tiup update dm
-tiup dm deploy dm-test v6.1.0 ./six-nodes-dm-hybrid.yaml --yes
+tiup dm deploy dm-test v6.1.1 ./six-nodes-dm-hybrid.yaml --yes
 tiup dm start dm-test
 sleep 10
 tiup dm display dm-test
