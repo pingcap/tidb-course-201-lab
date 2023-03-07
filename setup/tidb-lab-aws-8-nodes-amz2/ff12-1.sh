a56a74e7dc6278d7113ad59936f3fa7a0c84cd93
Chenhao Huang <chenhao.tony.huang@gmail.com>
2023-03-07 16:50:39 +1100

    minor update ff12-1.sh

--- setup/tidb-lab-aws-8-nodes-amz2/ff12-1.sh
+++ setup/tidb-lab-aws-8-nodes-amz2/ff12-1.sh
@@ -6,8 +6,8 @@ source .bash_profile
 source ./hosts-env.sh
 
 # Fast forward E12-1
-tiup install dm dmctl
-tiup update --self && tiup update dm && tiup update dm
+tiup install dm dmctl:v6.1.1
+# tiup update --self && tiup update dm && tiup update dm
 tiup dm deploy dm-test v6.1.1 ./six-nodes-dm-hybrid.yaml --yes
 tiup dm start dm-test
 sleep 10
