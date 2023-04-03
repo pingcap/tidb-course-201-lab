e6d915dd269325647059ac6a5004921f45c14a6c
Guanglei BAO <toddbgl@hotmail.com>
2023-04-04 00:08:02 +0800

    minor update

--- scripts/playground-start-e14-1.sh
+++ scripts/playground-start-e14-1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/.tiup/bin/tiup playground v6.6.0 --tag classroom-e14 --db 1 --pd 3 --kv 1 --tiflash 1
+~/.tiup/bin/tiup playground nightly --tag classroom-e14 --db 1 --pd 3 --kv 1 --tiflash 1
--- scripts/playground-start-e14-2.sh
+++ scripts/playground-start-e14-2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-~/.tiup/bin/tiup playground v6.6.0 --tag classroom-e14 --db 1 --pd 3 --kv 1 --tiflash 1 \
+~/.tiup/bin/tiup playground nightly --tag classroom-e14 --db 1 --pd 3 --kv 1 --tiflash 1 \
 --kv.config ./misc/tikv-e14.toml
