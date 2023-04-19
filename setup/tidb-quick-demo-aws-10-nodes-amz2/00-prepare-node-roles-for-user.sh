dc30fd8736d662ebc3f4d3ee301e11d2b7407600
Guanglei BAO <toddbgl@hotmail.com>
2023-04-19 09:23:54 +0800

    minor update

--- setup/tidb-quick-demo-aws-10-nodes-amz2/00-prepare-node-roles-for-user.sh
+++ setup/tidb-quick-demo-aws-10-nodes-amz2/00-prepare-node-roles-for-user.sh
@@ -129,14 +129,14 @@ HOST_DB2_PUBLIC_IP=`aws ec2 describe-instances \
 --region ${REGION_CODE}`
 
 # Node 10
-HOST_MONITOR_PRIVATE_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:student,Values=${USER}" "Name=tag:role,Values=monitor" "Name=tag:trainer,Values=${TRAINER}" \
+HOST_MONITOR1_PRIVATE_IP=`aws ec2 describe-instances \
+--filter "Name=instance-state-name,Values=running" "Name=tag:student,Values=${USER}" "Name=tag:role,Values=monitor1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PrivateIpAddress" \
 --output text \
 --region ${REGION_CODE}`
 
-HOST_MONITOR_PUBLIC_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=tag:student,Values=${USER}" "Name=tag:role,Values=monitor" "Name=tag:trainer,Values=${TRAINER}" \
+HOST_MONITOR1_PUBLIC_IP=`aws ec2 describe-instances \
+--filter "Name=instance-state-name,Values=running" "Name=tag:student,Values=${USER}" "Name=tag:role,Values=monitor1" "Name=tag:trainer,Values=${TRAINER}" \
 --query "Reservations[0].Instances[0].PublicIpAddress" \
 --output text \
 --region ${REGION_CODE}`
@@ -161,8 +161,8 @@ echo export HOST_KV3_PRIVATE_IP=${HOST_KV3_PRIVATE_IP} >> ./hosts-env.sh
 echo export HOST_KV3_PUBLIC_IP=${HOST_KV3_PUBLIC_IP} >> ./hosts-env.sh
 echo export HOST_TIFLASH1_PRIVATE_IP=${HOST_TIFLASH1_PRIVATE_IP} >> ./hosts-env.sh
 echo export HOST_TIFLASH1_PUBLIC_IP=${HOST_TIFLASH1_PUBLIC_IP} >> ./hosts-env.sh
-echo export HOST_MONITOR_PRIVATE_IP=${HOST_MONITOR_PRIVATE_IP} >> ./hosts-env.sh
-echo export HOST_MONITOR_PUBLIC_IP=${HOST_MONITOR_PUBLIC_IP} >> ./hosts-env.sh
+echo export HOST_MONITOR1_PRIVATE_IP=${HOST_MONITOR1_PRIVATE_IP} >> ./hosts-env.sh
+echo export HOST_MONITOR1_PUBLIC_IP=${HOST_MONITOR1_PUBLIC_IP} >> ./hosts-env.sh
 
 source ./hosts-env.sh
 
@@ -172,7 +172,7 @@ echo ssh -A ${HOST_PD2_PRIVATE_IP} > ./ssh-to-pd2.sh
 echo ssh -A ${HOST_PD3_PRIVATE_IP} > ./ssh-to-pd3.sh
 echo ssh -A ${HOST_DB1_PRIVATE_IP} > ./ssh-to-db1.sh
 echo ssh -A ${HOST_DB2_PRIVATE_IP} > ./ssh-to-db2.sh
-echo ssh -A ${HOST_MONITOR_PRIVATE_IP} > ./ssh-to-monitor.sh
+echo ssh -A ${HOST_MONITOR1_PRIVATE_IP} > ./ssh-to-monitor.sh
 echo ssh -A ${HOST_KV1_PRIVATE_IP} > ./ssh-to-kv1.sh
 echo ssh -A ${HOST_KV2_PRIVATE_IP} > ./ssh-to-kv2.sh
 echo ssh -A ${HOST_KV3_PRIVATE_IP} > ./ssh-to-kv3.sh
@@ -191,7 +191,7 @@ sed -i '' \
   -e "s/<HOST_KV3_PRIVATE_IP>/${HOST_KV3_PRIVATE_IP}/g" \
   -e "s/<HOST_DB1_PRIVATE_IP>/${HOST_DB1_PRIVATE_IP}/g" \
   -e "s/<HOST_DB2_PRIVATE_IP>/${HOST_DB2_PRIVATE_IP}/g" \
-  -e "s/<HOST_MONITOR_PRIVATE_IP>/${HOST_MONITOR_PRIVATE_IP}/g" \
+  -e "s/<HOST_MONITOR1_PRIVATE_IP>/${HOST_MONITOR1_PRIVATE_IP}/g" \
   ./ten-nodes.yaml 2>/dev/null
 
 # Copy hosts-env.sh to user home. It's also a safe operation if the PWD is user home. 
