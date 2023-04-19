4fda195a1db474ee06837fb5e26f44703101a9f6
Guanglei BAO <toddbgl@hotmail.com>
2023-04-19 13:09:00 +0800

    minor update

--- setup/tidb-quick-demo-aws-10-nodes-amz2/create-cluster-v650.sh
+++ setup/tidb-quick-demo-aws-10-nodes-amz2/create-cluster-v650.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Creating the TiDB cluster named tidb-demo, version 6.5.0
-~/.tiup/bin/tiup cluster deploy tidb-demo 6.5.0 ./eight-nodes-hybrid.yaml --yes
+~/.tiup/bin/tiup cluster deploy tidb-demo 6.5.0 ./ten-nodes.yaml --yes
