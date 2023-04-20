49b270e365102e94c596695e2ce3d4d270912fe3
Guanglei BAO <toddbgl@hotmail.com>
2023-04-20 16:13:22 +0800

    minor update

--- setup/tidb-quick-demo-aws-9-nodes-amz2/01-precheck-and-fix-nodes.sh
+++ setup/tidb-quick-demo-aws-9-nodes-amz2/01-precheck-and-fix-nodes.sh
@@ -2,8 +2,8 @@
 
 curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh
 
-~/.tiup/bin/tiup cluster check ten-nodes.yaml --apply && \
-~/.tiup/bin/tiup cluster check ten-nodes.yaml
+~/.tiup/bin/tiup cluster check nine-nodes.yaml --apply && \
+~/.tiup/bin/tiup cluster check nine-nodes.yaml
 
 echo "############################################################################################"
 echo "## It's safe to ignore disk and cpu-governor check failures in this classroom environment. #"
--- setup/tidb-quick-demo-aws-9-nodes-amz2/create-cluster-v651.sh
+++ setup/tidb-quick-demo-aws-9-nodes-amz2/create-cluster-v651.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+./01-precheck-and-fix-nodes.sh
+
 # Creating the TiDB cluster named tidb-demo, version 6.5.1
 ~/.tiup/bin/tiup cluster deploy tidb-demo 6.5.1 ./nine-nodes.yaml --yes
