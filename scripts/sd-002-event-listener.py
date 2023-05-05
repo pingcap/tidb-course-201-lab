67b27676f4244bd1374c200618eb1602402c63d9
Guanglei BAO <toddbgl@hotmail.com>
2023-05-05 14:28:35 +0800

    minor update

--- scripts/sd-002-event-listener.py
+++ scripts/sd-002-event-listener.py
@@ -7,14 +7,15 @@ ec2 = boto3.client("ec2", region_name=region)
 sts = boto3.client("sts", region_name=region)
 aws_account_id = sts.get_caller_identity()["Account"]
 queue_url = "https://sqs." + region + ".amazonaws.com/" + aws_account_id + "/demo-queue"
-res = sqs.receive_message(QueueUrl=queue_url)
 
 
 def check_queue():
+    res = sqs.receive_message(QueueUrl=queue_url)
     if "Messages" in res:
         for m in res["Messages"]:
             r_handle = m["ReceiptHandle"]
             m_body = m["Body"]
+            print("Got message:", m_body)
             node_type = m_body.split("::")[0]
             node_action = m_body.split("::")[1]
             node_address = m_body.split("::")[2]
@@ -25,17 +26,19 @@ def check_queue():
             if node_action == "scale-out":
                 for line in cluster_status:
                     if re.match(node_address, line):
-                        print("Node already joined cluster.")
+                        print(
+                            node_type, "node", node_address, "already joined cluster."
+                        )
                         sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=r_handle)
                         return
-                print("Adding node to cluster.")
+                print(node_type, "adding node", node_address, "to cluster.")
             elif node_action == "scale-in":
                 for line in cluster_status:
                     if re.match(node_address, line):
-                        print("Removing node from cluster.")
+                        print(node_type, "removing node", node_address, "from cluster.")
                         sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=r_handle)
                         return
-                print("Node already left cluster.")
+                print(node_type, "node", node_address, "already left cluster.")
             sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=r_handle)
 
 
