20a311b61db889484d616edccb0372317fb9db36
Guanglei BAO <toddbgl@hotmail.com>
2023-05-05 16:35:15 +0800

    minor update

--- scripts/sd-002-event-listener.py
+++ scripts/sd-002-event-listener.py
@@ -10,44 +10,6 @@ aws_account_id = sts.get_caller_identity()["Account"]
 queue_url = "https://sqs." + region + ".amazonaws.com/" + aws_account_id + "/demo-queue"
 
 
-def check_queue():
-    res = sqs.receive_message(QueueUrl=queue_url)
-    if "Messages" in res:
-        for m in res["Messages"]:
-            r_handle = m["ReceiptHandle"]
-            m_body = m["Body"]
-            print("Got message:", m_body)
-            node_type = m_body.split("::")[0]
-            node_action = m_body.split("::")[1]
-            node_address = m_body.split("::")[2]
-            cluster_status = subprocess.check_output(
-                ["/home/ec2-user/.tiup/bin/tiup", "cluster", "display", "tidb-demo"]
-            ).decode("utf-8")
-            print(cluster_status)
-            print("Node action:", node_action)
-            if node_action == "scale-out":
-                for line in cluster_status.split("\n"):
-                    if re.match(node_address, line):
-                        print(
-                            node_type, "node", node_address, "already joined cluster."
-                        )
-                        sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=r_handle)
-                        return
-                print(node_type, "adding node", node_address, "to cluster.")
-                if node_type == "TiDB":
-                    yaml = create_tidb_yaml(node_address)
-                    add_tidb_instance(yaml)
-                    register_tidb_instance_to_nlb(node_address)
-            elif node_action == "scale-in":
-                for line in cluster_status.split("\n"):
-                    if re.match(node_address, line):
-                        print(node_type, "removing node", node_address, "from cluster.")
-                        sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=r_handle)
-                        return
-                print(node_type, "node", node_address, "already left cluster.")
-            sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=r_handle)
-
-
 def create_tidb_yaml(tidb_address: str):
     template = """
     tidb_servers:
@@ -86,6 +48,44 @@ def register_tidb_instance_to_nlb(tidb_address: str):
             {"Id": tidb_address, "Port": 4000},
         ],
     )
+
+
+def check_queue():
+    res = sqs.receive_message(QueueUrl=queue_url)
+    if "Messages" in res:
+        for m in res["Messages"]:
+            r_handle = m["ReceiptHandle"]
+            m_body = m["Body"]
+            print("Got message:", m_body)
+            node_type = m_body.split("::")[0]
+            node_action = m_body.split("::")[1]
+            node_address = m_body.split("::")[2]
+            cluster_status = subprocess.check_output(
+                ["/home/ec2-user/.tiup/bin/tiup", "cluster", "display", "tidb-demo"]
+            ).decode("utf-8")
+            print(cluster_status)
+            print("Node action:", node_action)
+            if node_action == "scale-out":
+                for line in cluster_status.split("\n"):
+                    if re.match(node_address, line):
+                        print(
+                            node_type, "node", node_address, "already joined cluster."
+                        )
+                        sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=r_handle)
+                        return
+                print(node_type, "adding node", node_address, "to cluster.")
+                if node_type == "TiDB":
+                    yaml = create_tidb_yaml(node_address)
+                    add_tidb_instance(yaml)
+                    register_tidb_instance_to_nlb(node_address)
+            elif node_action == "scale-in":
+                for line in cluster_status.split("\n"):
+                    if re.match(node_address, line):
+                        print(node_type, "removing node", node_address, "from cluster.")
+                        sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=r_handle)
+                        return
+                print(node_type, "node", node_address, "already left cluster.")
+            sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=r_handle)
 
 
 while True:
--- setup/tidb-quick-demo-aws-9-nodes-amz2/start-cluster.sh
+++ setup/tidb-quick-demo-aws-9-nodes-amz2/start-cluster.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 ~/.tiup/bin/tiup cluster start tidb-demo
+sleep 3;
+./check_cluster.sh
