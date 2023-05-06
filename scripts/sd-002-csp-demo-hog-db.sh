5e43ef4cd8d92f3864f55426fcb6a532e74f0f98
Guanglei BAO <toddbgl@hotmail.com>
2023-05-06 11:42:15 +0800

    scripts for quick demo 002 ready

--- scripts/sd-002-csp-demo-hog-db.sh
+++ scripts/sd-002-csp-demo-hog-db.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-LB_DNS_NAME=`aws elbv2 describe-load-balancers --names demo-nlb --query "LoadBalancers[0].DNSName" --output text --region us-west-2`
+./sd-002-csp-demo-hog-db.sh &
+./sd-002-csp-demo-hog-db.sh &
 
-mysql -h ${LB_DNS_NAME} -P 4000 -uroot << EOF
-SELECT BENCHMARK(800000000,AES_ENCRYPT('hello','goodbye'));
-EOF
+echo 2 hoggers started.
