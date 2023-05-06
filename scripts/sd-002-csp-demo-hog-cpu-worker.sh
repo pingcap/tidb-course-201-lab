59f3ffac6c010912dcc92f131b69c878bbf15d8d
Guanglei BAO <toddbgl@hotmail.com>
2023-05-06 12:55:53 +0800

    minor fix

--- scripts/sd-002-csp-demo-hog-cpu-worker.sh
+++ scripts/sd-002-csp-demo-hog-cpu-worker.sh
@@ -2,4 +2,4 @@
 
 DB=${1}
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -A ec2-user@${DB} while true; do continue; done
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -A ec2-user@${DB} "while true; do continue; done"
