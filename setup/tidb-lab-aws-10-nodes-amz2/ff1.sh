cd3979f8d6d08a9cf506290e2737b939e0e1347d
Guanglei BAO <toddbgl@hotmail.com>
2023-05-09 22:48:54 +0800

    10 nodes 303 lab scripts added

--- setup/tidb-lab-aws-10-nodes-amz2/ff1.sh
+++ setup/tidb-lab-aws-10-nodes-amz2/ff1.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Fast forward E1
-./01-install-tiup.sh
-./02-precheck-and-fix-nodes.sh
+./01-precheck-and-fix-nodes.sh
 ./create-cluster-v650.sh
 ./start-cluster.sh
 sleep 20
--- setup/tidb-lab-aws-10-nodes-amz2/ff7.sh
+++ setup/tidb-lab-aws-10-nodes-amz2/ff7.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Fast forward E7
-./01-install-tiup.sh
-./02-precheck-and-fix-nodes.sh
+./01-precheck-and-fix-nodes.sh
 ./create-cluster-v651.sh
 ./start-cluster.sh
 sleep 20
