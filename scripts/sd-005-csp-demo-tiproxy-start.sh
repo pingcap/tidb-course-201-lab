c171936eaa7c5c2616cb89e6a502702c5a661776
Guanglei BAO <toddbgl@hotmail.com>
2023-05-12 18:48:23 +0800

    minor update

--- scripts/sd-005-csp-demo-tiproxy-start.sh
+++ scripts/sd-005-csp-demo-tiproxy-start.sh
@@ -4,7 +4,7 @@
 
 source ~/hosts-env.sh
 
-# Place X509 to TiDB nodes.
+# Place X509 to TiDB and TiProxy nodes.
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout key.pem -out cert.pem -subj "/CN=example.com"
 
 chmod 400 key.pem
@@ -13,6 +13,10 @@ scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no key.pem ec2-user
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no cert.pem ec2-user@${HOST_DB1_PRIVATE_IP}:/home/ec2-user/
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no key.pem ec2-user@${HOST_DB2_PRIVATE_IP}:/home/ec2-user/
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no cert.pem ec2-user@${HOST_DB2_PRIVATE_IP}:/home/ec2-user/
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no key.pem ec2-user@${HOST_TIPROXY1_PRIVATE_IP}:/home/ec2-user/
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no cert.pem ec2-user@${HOST_TIPROXY1_PRIVATE_IP}:/home/ec2-user/
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no key.pem ec2-user@${HOST_TIPROXY2_PRIVATE_IP}:/home/ec2-user/
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no cert.pem ec2-user@${HOST_TIPROXY2_PRIVATE_IP}:/home/ec2-user/
 
 # Place TiProxy configuration to TiProxy nodes.
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /home/ec2-user/tiproxy.toml ec2-user@${HOST_TIPROXY1_PRIVATE_IP}:/home/ec2-user/
