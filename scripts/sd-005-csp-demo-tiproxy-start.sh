e63c694ca64b184f9b5c576f8bcfb03ec2bcb818
Guanglei BAO <toddbgl@hotmail.com>
2023-05-12 16:33:17 +0800

    minor update

--- scripts/sd-005-csp-demo-tiproxy-start.sh
+++ scripts/sd-005-csp-demo-tiproxy-start.sh
@@ -2,7 +2,7 @@
 
 # This script is for TiDB Quick Demo 005 - Online Upgrade.
 
-source ./hosts-env.sh
+source ~/hosts-env.sh
 
 # Place X509 to TiDB nodes.
 openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout key.pem -out cert.pem -subj "/CN=example.com"
@@ -13,8 +13,8 @@ scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no key.pem ec2-user
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no cert.pem ec2-user@${HOST_DB2_PRIVATE_IP}:/home/ec2-user/
 
 # Place TiProxy configuration to TiProxy nodes.
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no tiproxy.toml ec2-user@${HOST_TIPROXY1_PRIVATE_IP}:/home/ec2-user/
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no tiproxy.toml ec2-user@${HOST_TIPROXY2_PRIVATE_IP}:/home/ec2-user/
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /home/ec2-user/tiproxy.toml ec2-user@${HOST_TIPROXY1_PRIVATE_IP}:/home/ec2-user/
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no /home/ec2-user/tiproxy.toml ec2-user@${HOST_TIPROXY2_PRIVATE_IP}:/home/ec2-user/
 
 # Start TiProxy layer
 ./sd-005-tiproxy.sh ${HOST_TIPROXY1_PRIVATE_IP} &
