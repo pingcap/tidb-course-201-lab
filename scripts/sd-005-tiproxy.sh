b3a9645cba857e25dbe351cacfbb56a81de8c383
Guanglei BAO <toddbgl@hotmail.com>
2023-05-16 08:08:35 +0800

    minor update

--- scripts/sd-005-tiproxy.sh
+++ scripts/sd-005-tiproxy.sh
@@ -2,4 +2,4 @@
 
 TIPROXY=${1}
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -A ec2-user@${TIPROXY} "TiProxy/bin/tiproxy --config=tiproxy.toml | grep -i router"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -A ec2-user@${TIPROXY} "TiProxy/bin/tiproxy --config=tiproxy.toml | tee -a tiproxy.log"
