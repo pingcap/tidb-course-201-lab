411fa0b7e69fcc1edf4bd54af2d77c78f58fb829
Chenhao Huang <chenhao.tony.huang@gmail.com>
2023-05-17 11:41:21 +1000

    004 demo adding column

---
+++ tidb-quick-demo/004-fast-adding-columns-online/setup/lib/_userdata.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo LANG=en_US.utf-8 >> /etc/environment
+echo LC_ALL=en_US.utf-8 >> /etc/environment
+rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+mkdir ~ec2-user/stage/
+cd ~ec2-user/stage/
+rm -rf tidb-course-201-lab/
+yum -y update
+yum -y install git numactl awscli
+yum -y install java-11-amazon-corretto
+wget https://pingcap-edu.s3.us-west-2.amazonaws.com/tidb-admin-dataset.zip
+git clone https://github.com/pingcap/tidb-course-201-lab.git
+REGION_CODE=`curl http://169.254.169.254/latest/meta-data/placement/region`
+echo export REGION_CODE=${REGION_CODE} > ~ec2-user/cloud-env.sh
+alias cp='cp'
+cp -R tidb-course-201-lab/setup/tidb-lab-aws-10-nodes-amz2/* ~ec2-user/
+cp -R tidb-course-201-lab/setup/tidb-lab-mysql-init-amz2/show-mysql-password.sh ~ec2-user/
+cp -R tidb-course-201-lab/scripts/ ~ec2-user/
+cd ~
+chown -R ec2-user:ec2-user ~ec2-user/*
+chmod +x ~ec2-user/*.sh
+chmod +x ~ec2-user/scripts/*.sh
+echo "if [ -f /home/ec2-user/hosts-env.sh ]; then" >> ~ec2-user/.bashrc
+echo "    source /home/ec2-user/hosts-env.sh" >> ~ec2-user/.bashrc
+echo "fi;" >> ~ec2-user/.bashrc
+chown ec2-user:ec2-user ~ec2-user/.bashrc
+su ec2-user -c "pip3 install boto3"
+wget https://dev.mysql.com/get/mysql57-community-release-el7-10.noarch.rpm
+yum -y install mysql57-community-release-el7-10.noarch.rpm
+yum -y install mysql-community-client
+wget https://pingcap-edu.s3.us-west-2.amazonaws.com/mysql-5.7.42-1.el7.x86_64.rpm-bundle.tar
+tar xvf mysql-5.7.42-1.el7.x86_64.rpm-bundle.tar
+rpm -ivh --force mysql-community-common-5.7.42-1.el7.x86_64.rpm mysql-community-server-5.7.42-1.el7.x86_64.rpm
