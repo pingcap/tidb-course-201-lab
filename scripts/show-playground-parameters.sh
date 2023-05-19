73494ec57ef972e0f1b91d6d0fbfc507ee797fef
Guanglei BAO <toddbgl@hotmail.com>
2023-05-19 18:37:49 +0800

    minor update

---
+++ scripts/show-playground-parameters.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SEARCH_PARAMETER=${1}
+
+echo -e "\nSearching parameter or system variables contain \"${SEARCH_PARAMETER}\"\n"
+
+TIDB_ADDRESS=127.0.0.1
+TIDB_PORT=4000
+
+echo -e "\n### Cluster Configurations:\n"
+mysql -h ${TIDB_ADDRESS} -uroot -P${TIDB_PORT} << EOF
+SHOW CONFIG WHERE name LIKE '%${SEARCH_PARAMETER}%';
+EOF
+
+echo -e "\n### System Variables:\n"
+mysql -h ${TIDB_ADDRESS} -uroot -P${TIDB_PORT} << EOF
+SHOW VARIABLES LIKE '%${SEARCH_PARAMETER}%';
+EOF
+
+echo
