6a6e74e35ab51f973a0a3bbab8da0a5dfe79d887
Guanglei BAO <toddbgl@hotmail.com>
2023-05-30 14:32:45 +0800

    minor fix

--- setup/tidb-lab-aws-10-nodes-amz2/ff8-2.sh
+++ setup/tidb-lab-aws-10-nodes-amz2/ff8-2.sh
@@ -18,14 +18,16 @@ mysql -h ${HOST_DB1_PRIVATE_IP} -uroot -P4000 << 'EOF'
 DROP DATABASE IF EXISTS my_db;
 EOF
 
+rm -rf /tmp/tidb_lightning_checkpoint.pb
+
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no solution-lightning-p2.toml ${HOST_PD1_PRIVATE_IP}:~/solution-lightning-p2.toml
 
 nohup tiup tidb-lightning:v6.5.1 -config solution-lightning-p1.toml > nohup.out & 
 sleep 10
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -A ${HOST_PD1_PRIVATE_IP} << 'EOF'
-nohup ~/.tiup/bin/tiup tidb-lightning:v6.5.1 -config solution-lightning-p2.toml > nohup.out &
+~/.tiup/bin/tiup tidb-lightning:v6.5.1 -config solution-lightning-p2.toml
 EOF
-sleep 10
+
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -A ${HOST_PD1_PRIVATE_IP} cat nohup.out
 
 mysql -h ${HOST_DB1_PRIVATE_IP} -uroot -P4000 << 'EOF'
