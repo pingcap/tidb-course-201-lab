2758dfc56ca8dbb691eaf2a3ffc01a496f7a278e
Chenhao Huang <chenhao.tony.huang@gmail.com>
2023-05-30 18:52:44 +1000

    minor update:

--- setup/tidb-lab-aws-10-nodes-amz2/ff12-1.sh
+++ setup/tidb-lab-aws-10-nodes-amz2/ff12-1.sh
@@ -8,7 +8,7 @@ source ./hosts-env.sh
 # Fast forward E12-1
 tiup install dm dmctl:v6.5.1
 # tiup update --self && tiup update dm && tiup update dm
-tiup dm deploy dm-test v6.5.1 ./solution-dm-topology-six-nodes.yaml--yes
+tiup dm deploy dm-test v6.5.1 ./solution-dm-topology-six-nodes.yaml --yes
 tiup dm start dm-test
 sleep 10
 tiup dm display dm-test
