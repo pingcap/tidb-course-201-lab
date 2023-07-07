4b6993d7a36ec75f095356de16ae5fd01f9e81e3
Guanglei BAO <toddbgl@hotmail.com>
2023-07-07 14:57:47 +0800

    minor update

--- setup/tidb-lab-aws-10-nodes-amz2/ff1.sh
+++ setup/tidb-lab-aws-10-nodes-amz2/ff1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Fast forward E1
-nohup ./create-cluster-v650.sh > nohup-tiup.log &
-tail -f nohup-tiup.log &
+# nohup ./create-cluster-v650.sh > nohup-tiup.log &
+# tail -f nohup-tiup.log &
+./create-cluster-v650.sh
