60b04cb29577ec36bec4bca7b8a23ead2d345874
Guanglei BAO <toddbgl@hotmail.com>
2023-07-09 22:12:58 +0800

    adding create-cluster-vinput.sh

---
+++ setup/tidb-lab-aws-1-node-amz2/create-cluster-vinput.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./01-precheck-and-fix-node.sh
+
+VERSION=${1}
+# Creating the TiDB cluster named tidb-test, version ${VERSION}
+~/.tiup/bin/tiup cluster deploy tidb-test ${VERSION} ./solution-topology-single-node.yaml --yes
+
+sleep 3;
+
+./start-cluster.sh
