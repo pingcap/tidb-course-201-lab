e0a6370b760df12a23c6330c2ed5cd1c03b12c5b
Guanglei BAO <toddbgl@hotmail.com>
2023-07-10 08:30:08 +0800

    change playground-start-rc.sh to use 1 PD only.

--- scripts/playground-start-rc.sh
+++ scripts/playground-start-rc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/.tiup/bin/tiup playground v7.1.0 --tag classroom-rc --db 1 --pd 3 --kv 1 --tiflash 0
+~/.tiup/bin/tiup playground v7.1.0 --tag classroom-rc --db 1 --pd 1 --kv 1 --tiflash 0
