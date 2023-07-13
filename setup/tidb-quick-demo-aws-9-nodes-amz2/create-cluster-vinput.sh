d4e5a8d6ef21a4adc42c07143aac3ea5a196fe2a
Guanglei BAO <toddbgl@hotmail.com>
2023-07-13 17:48:10 +0800

    minor update

--- setup/tidb-quick-demo-aws-9-nodes-amz2/create-cluster-vinput.sh
+++ setup/tidb-quick-demo-aws-9-nodes-amz2/create-cluster-vinput.sh
@@ -3,8 +3,8 @@
 ./01-precheck-and-fix-nodes.sh
 
 VERSION=${1}
-# Creating the TiDB cluster named tidb-test, version ${VERSION}
-~/.tiup/bin/tiup cluster deploy tidb-test ${VERSION} ./nine-nodes.yaml --yes
+# Creating the TiDB cluster named tidb-demo, version ${VERSION}
+~/.tiup/bin/tiup cluster deploy tidb-demo ${VERSION} ./nine-nodes.yaml --yes
 
 sleep 3;
 
