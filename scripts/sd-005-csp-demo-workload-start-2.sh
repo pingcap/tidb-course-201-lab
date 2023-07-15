6f32a6528dfa534fde7fd24cffc6cac69547d8c4
Guanglei BAO <toddbgl@hotmail.com>
2023-07-15 17:02:52 +0800

    minor update

--- scripts/sd-005-csp-demo-workload-start-2.sh
+++ scripts/sd-005-csp-demo-workload-start-2.sh
@@ -5,7 +5,6 @@ source ../cloud-env.sh
 LB_DNS_NAME=`aws elbv2 describe-load-balancers --names demo-nlb --query "LoadBalancers[0].DNSName" --output text --region ${REGION_CODE}`
 
 mysql -h ${LB_DNS_NAME} -P 6000 -uroot << EOF
-SET CONFIG PD replication.max-replicas=3;
 DROP TABLE IF EXISTS test.dummy; 
 CREATE TABLE test.dummy (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(80), event JSON, tidb_instance VARCHAR(30));
 EOF
