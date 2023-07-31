c1c3e4344c511eadd2513a82c8030ee750286278
Guanglei BAO <toddbgl@hotmail.com>
2023-07-31 16:19:18 +0800

    updated show-public-dashboad-url.sh to get dashboad URL directly.

--- setup/tidb-lab-aws-1-node-amz2/show-public-dashboad-url.sh
+++ setup/tidb-lab-aws-1-node-amz2/show-public-dashboad-url.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 source ~/cloud-env.sh
 
-UI_PRIVATE_IP=`~/.tiup/bin/tiup cluster display tidb-test | grep UI | grep pd | awk -F" " '{print $3}' | xargs`
+URL=`~/.tiup/bin/tiup cluster display tidb-test | grep 'Dashboard URL:' | awk -F": " '{print $2}' | xargs`
 
-UI_PUBLIC_IP=`aws ec2 describe-instances \
---filter "Name=instance-state-name,Values=running" "Name=network-interface.addresses.private-ip-address,Values=${UI_PRIVATE_IP}" \
---query "Reservations[0].Instances[0].PublicIpAddress" \
---output text \
---region ${REGION_CODE}`
-
-echo http://${UI_PUBLIC_IP}:2379/dashboard
+echo ${URL}
