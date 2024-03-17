e263e29a5eed7a56f9fce8190fb15a5f00f9ff1b
Guanglei BAO <toddbgl@hotmail.com>
2024-03-17 23:10:27 +0800

    fff10.sh added

---
+++ setup/tidb-lab-aws-10-nodes-amz2/fff10.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source .bash_profile
+source ./hosts-env.sh
+
+./fff9.sh
+
+~/.tiup/bin/tiup install br:v6.5.1
+unzip -u stage/tidb-admin-dataset.zip -d stage/
+
+ssh ${HOST_KV1_PRIVATE_IP} unzip -u stage/tidb-admin-dataset.zip -d stage/
+
+mysql -h ${HOST_DB1_PRIVATE_IP} -uroot -P4000 << 'EOF'
+DROP DATABASE IF EXISTS emp;
+CREATE USER ltask@'%' IDENTIFIED BY 'q1w2e3R4_';
+GRANT ALL PRIVILEGES ON *.* TO ltask@'%';
+EOF
+
+nohup ~/.tiup/bin/tiup tidb-lightning:v6.5.1 -config solution-lightning-init.toml > nohup.out &
+
+ssh ${HOST_KV1_PRIVATE_IP} mkdir /tmp/backup
+ssh ${HOST_KV2_PRIVATE_IP} mkdir /tmp/backup
+ssh ${HOST_KV3_PRIVATE_IP} mkdir /tmp/backup
+
+~/.tiup/bin/tiup br:v6.5.1 backup full --pd "${HOST_PD1_PRIVATE_IP}:2379" --storage "local:///tmp/backup" --ratelimit 128 --log-file backupfull.log
+
+ssh ${HOST_KV1_PRIVATE_IP} find /tmp/backup/
+
+ls /tmp/backup/
