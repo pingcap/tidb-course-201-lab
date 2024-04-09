509587ed195bb1a778018145eadf953940830379
Guanglei BAO <toddbgl@hotmail.com>
2024-04-09 10:23:52 +0800

    adding setup scripts to demo006

---
+++ setup/tidb-quick-demo-aws-9-nodes-amz2/emp-schema-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ./hosts-env.sh
+
+mysql -h ${HOST_DB1_PRIVATE_IP} -uroot -P4000 << 'EOFX'
+CREATE USER ltask@'%' IDENTIFIED BY 'q1w2e3R4_';
+GRANT ALL PRIVILEGES ON *.* TO ltask@'%';
+DROP DATABASE IF EXISTS emp;
+CREATE DATABASE emp;
+USE emp;
+CREATE TABLE `sal` ( `emp_no` INT(11) NOT NULL, `salary` INT(11) NOT NULL, `from_date` DATE NOT NULL, `to_date` DATE NOT NULL, PRIMARY KEY (`emp_no`,`from_date`));
+EOFX
+
+./tiup/bin/tiup install tidb-lightning:v7.1.0
+
+./tiup/bin/tiup tidb-lightning:v7.1.0 -config solution-lightning-csv.toml
