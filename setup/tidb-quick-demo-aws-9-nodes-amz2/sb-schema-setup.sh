dc1f2508606a699d3a274ec7ebdf48b9cc4aeef9
Guanglei BAO <toddbgl@hotmail.com>
2024-04-09 15:41:05 +0800

    minor update

--- setup/tidb-quick-demo-aws-9-nodes-amz2/sb-schema-setup.sh
+++ setup/tidb-quick-demo-aws-9-nodes-amz2/sb-schema-setup.sh
@@ -9,3 +9,15 @@ CREATE USER 'userB'@'%' IDENTIFIED BY 'tidb';
 GRANT ALL PRIVILEGES ON *.*  TO 'userA'@'%';
 GRANT ALL PRIVILEGES ON *.*  TO 'userB'@'%';
 EOFX
+
+sysbench '/usr/local/share/sysbench/oltp_read_only.lua' \
+--mysql-host=${HOST_DB1_PRIVATE_IP} \
+--mysql-port=4000 \
+--mysql-user=root \
+--mysql-db=sbtest \
+--percentile=99 \
+--threads=10 \
+--tables=32 \
+--table-size=100000 \
+--report-interval=2 \
+--db-driver=mysql prepare
