37303d976f7e175385762628ae146514cdd9bb33
Guanglei BAO <toddbgl@hotmail.com>
2024-04-09 11:11:54 +0800

    minor update

--- setup/tidb-quick-demo-aws-9-nodes-amz2/sb-schema-setup.sh
+++ setup/tidb-quick-demo-aws-9-nodes-amz2/sb-schema-setup.sh
@@ -6,6 +6,6 @@ mysql -h ${HOST_DB1_PRIVATE_IP} -uroot -P4000 << 'EOFX'
 CREATE DATABASE sbtest;
 CREATE USER 'userA'@'%' IDENTIFIED BY 'tidb';
 CREATE USER 'userB'@'%' IDENTIFIED BY 'tidb';
-GRANT ALL PRIVILEGES ON *.*  TO 'user1'@'%';
-GRANT ALL PRIVILEGES ON *.*  TO 'user2'@'%';
+GRANT ALL PRIVILEGES ON *.*  TO 'userA'@'%';
+GRANT ALL PRIVILEGES ON *.*  TO 'userB'@'%';
 EOFX
