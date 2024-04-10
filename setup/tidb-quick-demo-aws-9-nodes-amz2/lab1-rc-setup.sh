b6b3b0c74d65dd1d3c30ea3f58593f79078655f1
Guanglei BAO <toddbgl@hotmail.com>
2024-04-10 12:27:22 +0800

    minor update

--- setup/tidb-quick-demo-aws-9-nodes-amz2/lab1-rc-setup.sh
+++ setup/tidb-quick-demo-aws-9-nodes-amz2/lab1-rc-setup.sh
@@ -5,6 +5,8 @@ source ./hosts-env.sh
 # For cluster
 export MYSQL_PS1="tidb:db1> "
 mysql -h ${HOST_DB1_PRIVATE_IP} -P 4000 -u root --verbose << 'EOFX'
+ALTER USER userA RESOURCE GROUP default;
+ALTER USER userB RESOURCE GROUP default;
 DROP RESOURCE GROUP IF EXISTS rg1;
 DROP RESOURCE GROUP IF EXISTS rg2;
 CREATE RESOURCE GROUP rg1 RU_PER_SEC=50000 PRIORITY=HIGH;
