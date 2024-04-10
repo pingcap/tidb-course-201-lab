bbf9b223a3a31f84c6994bbd55dfad15c5919ab0
Guanglei BAO <toddbgl@hotmail.com>
2024-04-10 17:01:41 +0800

    minor update

--- setup/tidb-quick-demo-aws-9-nodes-amz2/lab1-workload-userA.sh
+++ setup/tidb-quick-demo-aws-9-nodes-amz2/lab1-workload-userA.sh
@@ -2,4 +2,16 @@
 
 source ./hosts-env.sh
 
-python3 userA.py 50
+sysbench '/usr/local/share/sysbench/oltp_read_only.lua' \
+--mysql-host=${HOST_DB1_PRIVATE_IP} \
+--mysql-port=4000 \
+--mysql-user=userA \
+--mysql-db=sbtest \
+--percentile=99 \
+--mysql-password=tidb \
+--time=300 \
+--threads=10 \
+--tables=32 \
+--table-size=100000 \
+--report-interval=2 run
+
