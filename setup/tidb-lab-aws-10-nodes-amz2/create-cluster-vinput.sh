3b4fde2dd652023bb256e7422e870392735059d8
Guanglei BAO <toddbgl@hotmail.com>
2024-05-25 17:58:45 +0800

    minor update

--- setup/tidb-lab-aws-10-nodes-amz2/create-cluster-vinput.sh
+++ setup/tidb-lab-aws-10-nodes-amz2/create-cluster-vinput.sh
@@ -7,7 +7,7 @@ REGION_NAME=${2}
 
 # Creating the TiDB cluster named tidb-test, version ${VERSION}
 ~/.tiup/bin/tiup mirror set tidb-community-server-${VERSION}-linux-amd64
-~/.tiup/bin/tiup cluster deploy tidb-test ${VERSION} ./solution-topology-ten-nodes.yaml --user ec2-user -i /home/ec2-user/.ssh/pe-class-key-${REGION_NAME} --yes
+~/.tiup/bin/tiup cluster deploy tidb-test ${VERSION} ./solution-topology-ten-nodes.yaml --user ec2-user -i /home/ec2-user/.ssh/pe-class-key-${REGION_NAME}.pem --yes
 sleep 3;
 
 ~/.tiup/bin/tiup cluster start tidb-test
