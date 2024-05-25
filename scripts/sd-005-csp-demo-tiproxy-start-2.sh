f0e36179405f87f63ec374ce23f3f5d9b11ef836
Guanglei BAO <toddbgl@hotmail.com>
2024-05-25 21:32:52 +0800

    minor update

--- scripts/sd-005-csp-demo-tiproxy-start-2.sh
+++ scripts/sd-005-csp-demo-tiproxy-start-2.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+REGION_NAME=${1}
 
 # This script is for TiDB Quick Demo 005 - Online Upgrade.
 
 source ~/hosts-env.sh
 
 # Start TiProxy layer
-./sd-005-tiproxy.sh ${HOST_TIPROXY1_PRIVATE_IP} &
-./sd-005-tiproxy.sh ${HOST_TIPROXY2_PRIVATE_IP} &
+./sd-005-tiproxy.sh ${HOST_TIPROXY1_PRIVATE_IP} ${REGION_NAME} &
+./sd-005-tiproxy.sh ${HOST_TIPROXY2_PRIVATE_IP} ${REGION_NAME} &
 
 echo TiProxy Starting ...
 sleep 3;
--- scripts/sd-005-tiproxy.sh
+++ scripts/sd-005-tiproxy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 TIPROXY=${1}
+REGION_NAME=${2}
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -A ec2-user@${TIPROXY} "TiProxy/bin/tiproxy --config=tiproxy.toml"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i ~/.ssh/pe-class-key-${REGION_NAME}.pem ec2-user@${TIPROXY} "TiProxy/bin/tiproxy --config=tiproxy.toml"
